60e21a1ba132825199e54884b0cfc26e65d7dd0e
DhineshKs <dhineshksd1997@gmail.com>
2020-03-31 12:02:48 +0530

    read message on basis of batch size

--- kafka-singlenode-kafka-to-mongo/consume.go
+++ kafka-singlenode-kafka-to-mongo/consume.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -13,5 +14,18 @@ func main() {
 
 	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
 	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	defer conn.Close()
 
+	//setting 10kb minimum and 1mb maximum
+	batch := conn.ReadBatch(10e3, 1e6)
+	defer batch.Close()
+	//10kb per message
+	b := make([]byte, 10e3)
+	for {
+		_, err := batch.Read(b)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(b))
+	}
 }
